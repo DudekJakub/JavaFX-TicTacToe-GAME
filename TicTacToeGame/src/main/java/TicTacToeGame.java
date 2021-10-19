a3a3e08bda4761089bdc1feab2c51ff46a45ed0c
Jakub Dudek <jakub.dudek94@gmail.com>
2021-10-19 11:37:25 +0200

    Second Commit

--- TicTacToeGame/src/main/java/TicTacToeGame.java
+++ TicTacToeGame/src/main/java/TicTacToeGame.java
@@ -4,26 +4,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class TicTacToeApp extends Application {
+public class TicTacToeGame extends Application {
 
     private Parent drawContent() {
         Pane root = new Pane();
-        root.setPrefSize(600,600);
+        root.setPrefSize(600, 600);
 
         return root;
     }
-
-
 
     @Override
     public void start(Stage mainStage) throws Exception {
         mainStage.setScene(new Scene(drawContent()));
         mainStage.show();
-
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
